8da5a5342ff089e2bcd695769587d838d0c706cf
Peter Maselkowski <pmaselkowski@gmail.com>
2016-05-13 10:18:37 +0200

    ^ dep - removed mongofill
    * Class headers/toc

--- src/HttpSession.php
+++ src/HttpSession.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * This software package is licensed under AGPL or Commercial license.
+ * This software package is licensed under AGPL, Commercial license.
  *
- * @package maslosoft/mangan
- * @licence AGPL or Commercial
+ * @package maslosoft/mangan-yii
+ * @licence AGPL, Commercial
  * @copyright Copyright (c) Piotr Masełkowski <pmaselkowski@gmail.com>
  * @copyright Copyright (c) Maslosoft
  * @copyright Copyright (c) Others as mentioned in code
- * @link http://maslosoft.com/mangan/
+ * @link http://maslosoft.com/mangan-yii/
  */
 
 namespace Maslosoft\ManganYii;
